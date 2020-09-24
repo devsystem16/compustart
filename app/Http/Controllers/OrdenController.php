16a1b9cf608b4c7f88f9d25effba6d5f2cde4b34
mychael <desarrollos16@hotmail.com>
2020-09-23 22:22:35 -0500

    Usar Sp en lugar de consultas

--- app/Http/Controllers/OrdenController.php
+++ app/Http/Controllers/OrdenController.php
@@ -13,46 +13,14 @@ class OrdenController extends Controller
     }
 
     public function reporteGarantia ($inicio  , $fin ){
-        return DB::select ("select  
-		o.fecha  as FECHA
-		, o.serie AS SERIE
-        , o.falla AS FALLA
-        , o.informeTecnico  AS TRABAJO
-        , UPPER( concat(ifnull( c.nombre1,'')  ,' ' , ifnull( c.nombre2,'')  ,' ' ,  ifnull( c.apellidoPaterno,'') , ' ' ,ifnull(  c.apellidoMaterno,'') ) ) AS CLIENTE
-        , eo.descripcion  AS 'ESTADO'
-									from orden o 
-                                    inner join cliente c on c.IDCliente = o.IDCliente
-                                    inner join garantia g on g.IDGarantia = o.IDGarantia
-                                    inner join estadoorden eo on o.IDEstadoOrden = eo.IDEstadoOrden
-                                    where g.sinCosto = 1 
-                                    and   g.mostrarExcel = 1
-                                   and  DATE_FORMAT(o.fecha,'%Y-%m-%d') BETWEEN  '".$inicio."' and  '".$fin."'
-                                   and o.IDStatus = (select  IDStatus from status where descripcion='Activo')
-                                  order by o.fecha desc;");
+        return DB::select ("CALL reporteGarantia ( '".$inicio."' , '".$fin."' );");
     }
 
-public function reporteGarantiaSoloHaitech ($inicio  , $fin ){
-        return DB::select ("select  
-	 
-        o.serie AS 'NRO DE SERIE'
-      , c.cedula as CEDULA
-      , UPPER( concat( ifnull( c.nombre1,'')  ,' ' , ifnull( c.nombre2 ,'')  ,' ' , ifnull( c.apellidoPaterno ,'') , ' ' , ifnull(c.apellidoMaterno,'') ) )AS CUSTODIO
-      , o.falla AS INCIDENTE
-      , o.informeTecnico  AS SOLUCION
-      , eo.descripcion as ESTADO
-      
-                                  from orden o 
-                                  inner join cliente c on c.IDCliente = o.IDCliente
-                                  inner join garantia g on g.IDGarantia = o.IDGarantia
-                                  INNER JOIN usuario u on u.IDUsuario   = o.IDUsuario
-                                  INNER JOIN estadoorden eo on eo.IDEstadoOrden  = o.IDEstadoOrden
-                                  where g.sinCosto = 1
-                                  and   g.mostrarExcel = 1
-                                  and  DATE_FORMAT(o.fecha,'%Y-%m-%d') BETWEEN  '".$inicio."' and  '".$fin."'
-                                  and o.IDStatus = (select  IDStatus from status where descripcion='Activo')
-                                  order by o.fecha desc;");
+    public function reporteGarantiaSoloHaitech ($inicio  , $fin ){
+        return DB::select  ("CALL reporteGarantiaSoloHaitech ( '".$inicio."' , '".$fin."' );");
     }
-public function getOrden ($numeroOrden){
+    
+    public function getOrden ($numeroOrden){
         return DB::select (" call getOrden ('".$numeroOrden."') ;");
     }
 
